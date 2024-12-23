4177f502e218a7a5010e00d5044e48c4cb5e8bdf
nikolaevna.spb <nikolaevna.spb@mail.ru>
2024-12-23 23:13:05 +0300

    Второе задание tabs выполнено

---
+++ event-object/tabs/task.js
@@ -0,0 +1,15 @@
+const tabs = [...document.querySelectorAll(".tab")];
+const tabsContent = [...document.querySelectorAll(".tab__content")];
+
+const tabActive = "tab_active";
+const contentActive = "tab__content_active";
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach(tab => tab.classList.remove(tabActive));
+    tabsContent.forEach(content => content.classList.remove(contentActive));
+
+    tab.classList.add(tabActive);
+    tabsContent[index].classList.add(contentActive);
+  });
+});
