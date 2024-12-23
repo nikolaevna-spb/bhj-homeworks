5eb65d16f39b020b7c505cb0075333cba6982e4b
nikolaevna.spb <nikolaevna.spb@mail.ru>
2024-12-23 22:49:25 +0300

    Первое задание dropdown выполнено

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,16 @@
+const value = document.querySelector(".dropdown__value");
+const list = document.querySelector(".dropdown__list");
+const items = [...document.querySelectorAll(".dropdown__item")];
+
+value.addEventListener("click", () => {
+    list.classList.toggle("dropdown__list_active");
+    return false;
+});
+
+items.forEach((item) => {
+    item.addEventListener("click", () => {
+        value.textContent = item.textContent;
+        list.classList.remove("dropdown__list_active");
+        return false;
+    });
+});
