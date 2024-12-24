6c65ffc139653b4e5accf26588999bd752637130
nikolaevna.spb <nikolaevna.spb@mail.ru>
2024-12-24 23:49:14 +0300

    Второе задание ads выполнено

---
+++ dom/ads/task.js
@@ -0,0 +1,20 @@
+const spans = [...document.querySelectorAll(".rotator__case")];
+
+function rotation(spans) {
+    for (let i = 0; i < spans.length; i++) {
+        if (spans[i].classList.contains("rotator__case_active")) {
+            spans[i].classList.remove("rotator__case_active");
+            nextSpan = spans[i].nextElementSibling;
+            if (!nextSpan) {
+                spans[0].classList.add("rotator__case_active");
+            } else {
+                nextSpan.classList.add("rotator__case_active");
+                break;
+            }
+        }
+    }
+}
+
+setInterval(() => {
+    rotation(spans);
+}, 1000);
